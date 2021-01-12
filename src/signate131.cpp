92e6ebf6300e51610fdc0bd42f9d9fd230a5f913
octpath <t.tsutaoka@gmail.com>
2021-01-12 09:34:01 +0900

    mod proc1

--- src/signate131.cpp
+++ src/signate131.cpp
@@ -8,14 +8,14 @@ namespace py = pybind11;
 
 // naive
 std::vector<std::map<int, std::vector<std::vector<int> > > > proc(py::array_t<float> input){
-    auto r = input.unchecked<3>();
+    auto data = input.unchecked<3>();
     std::vector<std::map<int, std::vector<std::vector<int> > > > results(3);
-    for (py::ssize_t cat = 0; cat < 3; cat++){
-        for (py::ssize_t row = 0; row < r.shape(0); row++){
+    for (py::ssize_t cat = 0; cat < 3; ++cat){
+        for (py::ssize_t row = 0; row < data.shape(0); ++row){
             bool flag = false;
             int start = -1;
-            for (py::ssize_t col = 0; col < r.shape(1); col++){
-                if (r(row, col, cat) > 0.5){
+            for (py::ssize_t col = 0; col < data.shape(1); ++col){
+                if (data(row, col, cat) > 0.5){
                     if (!flag) {
                         flag = true;
                         start = col;
@@ -23,9 +23,12 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc(py::array_t<fl
                 } else {
                     if (flag) {
                         flag = false;
-                        results[cat][row].push_back(std::vector<int>{start, (int)(col - 1)});
+                        results[cat][row].push_back(std::vector<int>{start, (int)(col) - 1});
                     }
                 }
+            }
+            if (flag) {
+                results[cat][row].push_back(std::vector<int>{start, (int)(data.shape(1)) - 1});
             }
         }
     }
@@ -36,12 +39,12 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc(py::array_t<fl
 std::vector<std::map<int, std::vector<std::vector<int> > > > proc2(py::array_t<float> input){
     auto data = input.unchecked<3>();
     std::vector<std::map<int, std::vector<std::vector<int> > > > results(3);
-    for (py::ssize_t cat = 0; cat < 3; cat++){
-        for (py::ssize_t row = 0; row < data.shape(0); row++){
+    for (py::ssize_t cat = 0; cat < 3; ++cat){
+        for (py::ssize_t row = 0; row < data.shape(0); ++row){
             bool flag = false;
             int start = -1;
-            for (py::ssize_t col = 0; col < data.shape(1); col++){
-                if (data(row, col, cat) > 0.5){
+            for (py::ssize_t col = 0; col < data.shape(1); ++col){
+                if (data(row, col, cat) >= 0.5){
                     if (!flag) {
                         flag = true;
                         start = col;
@@ -49,15 +52,15 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc2(py::array_t<f
                 } else {
                     if (flag) {
                         flag = false;
-                        for (int scale_f=0; scale_f<20; scale_f++){
-                            results[cat][20*row+scale_f].push_back(std::vector<int>{20*start, 20*((int)col - 1)});
+                        for (int scale_f=0; scale_f<20; ++scale_f){
+                            results[cat][20*row+scale_f].push_back(std::vector<int>{20*start, 20*((int)(col) - 1)});
                         }
                     }
                 }
             }
             if (flag) {
-                for (int scale_f=0; scale_f<20; scale_f++){
-                    results[cat][20*row+scale_f].push_back(std::vector<int>{20*start, 20*((int)data.shape(1) - 1)});
+                for (int scale_f=0; scale_f<20; ++scale_f){
+                    results[cat][20*row+scale_f].push_back(std::vector<int>{20*start, 20*((int)(data.shape(1)) - 1)});
                 }
             }
         }
@@ -74,7 +77,8 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc3(py::array_t<f
             for (py::ssize_t col = 0; col < r.shape(1); col++){
                 if (r(row, col, cat) > 0.5){
                     for (int scale_f=0; scale_f<20; scale_f++){
-                        results[cat][20*row+scale_f].push_back(std::vector<int>{20*(int)(col), 20*(int)(col)+19});
+                        results[cat][20*row+scale_f].push_back(
+                            std::vector<int>{20*(int)(col), 20*(int)(col)+19});
                     }
                 }
             }
