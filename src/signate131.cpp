ed23c2354ad1dc05ec8189c94102f76b857f73a6
octpath <t.tsutaoka@gmail.com>
2021-01-12 15:19:13 +0900

    add proc4 (1cat) and mod proc3

--- src/signate131.cpp
+++ src/signate131.cpp
@@ -87,9 +87,41 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc3(py::array_t<f
     return results;
 }
 
+// x20, 1cat
+std::vector<std::map<int, std::vector<std::vector<int> > > > proc4(py::array_t<float> input){
+    auto data = input.unchecked<2>();
+    std::map<int, std::vector<std::vector<int> > > results;
+    for (py::ssize_t row = 0; row < data.shape(0); ++row){
+        bool flag = false;
+        int start = -1;
+        for (py::ssize_t col = 0; col < data.shape(1); ++col){
+            if (data(row, col) >= 0.5){
+                if (!flag) {
+                    flag = true;
+                    start = col;
+                }
+            } else {
+                if (flag) {
+                    flag = false;
+                    for (int scale_f=0; scale_f<20; ++scale_f){
+                        results[20*row+scale_f].push_back(std::vector<int>{20*start, 20*(int)(col) - 1});
+                    }
+                }
+            }
+        }
+        if (flag) {
+            for (int scale_f=0; scale_f<20; ++scale_f){
+                results[20*row+scale_f].push_back(std::vector<int>{20*start, 20*(int)(data.shape(1)) - 1});
+            }
+        }
+    }
+    return results;
+}
+
 PYBIND11_MODULE(signate131, m){
     m.doc() = "signate131 postprocess implementations";
     m.def("proc", &proc, "naive");
     m.def("proc2", &proc2, "x20");
-    m.def("proc3", &proc2, "x20 for each grids");
+    m.def("proc3", &proc3, "x20 for each grids");
+    m.def("proc4", &proc4, "x20 1cat");
 }
