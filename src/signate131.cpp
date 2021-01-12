ebb279944f50f0ed9bd8994ae79e0683c53ef472
octpath <t.tsutaoka@gmail.com>
2021-01-12 15:42:30 +0900

    proc2_with_scale

--- src/signate131.cpp
+++ src/signate131.cpp
@@ -68,50 +68,31 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc2(py::array_t<f
     return results;
 }
 
-// x20, grid
-std::vector<std::map<int, std::vector<std::vector<int> > > > proc3(py::array_t<float> input){
-    auto r = input.unchecked<3>();
+// proc2 with scale
+std::vector<std::map<int, std::vector<std::vector<int> > > > proc2_with_scale(py::array_t<float> input, int scale){
+    auto data = input.unchecked<3>();
     std::vector<std::map<int, std::vector<std::vector<int> > > > results(3);
-    for (py::ssize_t cat = 0; cat < 3; cat++){
-        for (py::ssize_t row = 0; row < r.shape(0); row++){
-            for (py::ssize_t col = 0; col < r.shape(1); col++){
-                if (r(row, col, cat) >= 0.5){
-                    for (int scale_f=0; scale_f<20; scale_f++){
-                        results[cat][20*row+scale_f].push_back(
-                            std::vector<int>{20*(int)(col), 20*(int)(col)+19});
-                    }
-                }
-            }
-        }
-    }
-    return results;
-}
-
-// x20, 1cat
-std::map<int, std::vector<std::vector<int> > > proc4(py::array_t<float> input){
-    auto data = input.unchecked<2>();
-    std::map<int, std::vector<std::vector<int> > > results;
-    for (py::ssize_t row = 0; row < data.shape(0); ++row){
-        bool flag = false;
-        int start = -1;
-        for (py::ssize_t col = 0; col < data.shape(1); ++col){
-            if (data(row, col) >= 0.5){
-                if (!flag) {
+    for (py::ssize_t cat = 0; cat < 3; ++cat){
+        for (py::ssize_t row = 0; row < data.shape(0); ++row){
+            bool flag = false;
+            int start = -1;
+            for (py::ssize_t col = 0; col < data.shape(1); ++col){
+                auto ref_val = data(row, col, cat)
+                if ((ref_val >= 0.5) && (!flag)) {
                     flag = true;
                     start = col;
-                }
-            } else {
-                if (flag) {
+                } 
+                else if ((ref_val < 0.5) && (flag)) {
                     flag = false;
-                    for (int scale_f=0; scale_f<20; ++scale_f){
-                        results[20*row+scale_f].push_back(std::vector<int>{20*start, 20*(int)(col) - 1});
+                    for (int scale_f = 0; scale_f < scale; ++scale_f){
+                        results[cat][scale * row + scale_f].push_back(std::vector<int>{scale * start, scale * (int)(col) - 1});
                     }
                 }
             }
-        }
-        if (flag) {
-            for (int scale_f=0; scale_f<20; ++scale_f){
-                results[20*row+scale_f].push_back(std::vector<int>{20*start, 20*(int)(data.shape(1)) - 1});
+            if (flag) {
+                for (int scale_f = 0; scale_f < scale; ++scale_f){
+                    results[cat][scale * row + scale_f].push_back(std::vector<int>{scale * start, scale * (int)(data.shape(1)) - 1});
+                }
             }
         }
     }
@@ -122,6 +103,5 @@ PYBIND11_MODULE(signate131, m){
     m.doc() = "signate131 postprocess implementations";
     m.def("proc", &proc, "naive");
     m.def("proc2", &proc2, "x20");
-    m.def("proc3", &proc3, "x20 for each grids");
-    m.def("proc4", &proc4, "x20 1cat");
+    m.def("proc2_with_scale", &proc2_with_scale, "with scale");
 }
