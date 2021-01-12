da54d76b026612808dce45981fb1c313802eb83f
octpath <t.tsutaoka@gmail.com>
2021-01-12 15:51:28 +0900

    proc3

--- src/signate131.cpp
+++ src/signate131.cpp
@@ -99,9 +99,42 @@ std::vector<std::map<int, std::vector<std::vector<int> > > > proc2_with_scale(py
     return results;
 }
 
+// proc3
+std::vector<std::map<int, std::vector<std::vector<int> > > > proc3(py::array_t<float> input){
+    auto data = input.unchecked<3>();
+    std::vector<std::map<int, std::vector<std::vector<int> > > > results(3);
+    for (py::ssize_t cat = 0; cat < 3; ++cat){
+        for (py::ssize_t row = 0; row < data.shape(0); ++row){
+            bool flag = false;
+            int start = -1;
+            for (py::ssize_t col = 0; col < data.shape(1); ++col){
+                auto ref_val = data(row, col, cat);
+                if ((ref_val >= 0.5) && (!flag)) {
+                    flag = true;
+                    start = col;
+                } 
+                else if ((ref_val < 0.5) && (flag)) {
+                    flag = false;
+                    for (int scale_f = 0; scale_f < 20; ++scale_f){
+                        results[cat][20 * row + scale_f].push_back(std::vector<int>{20 * start, 20 * (int)(col) - 1});
+                    }
+                }
+            }
+            if (flag) {
+                for (int scale_f = 0; scale_f < 20; ++scale_f){
+                    results[cat][20 * row + scale_f].push_back(std::vector<int>{20 * start, 20 * (int)(data.shape(1)) - 1});
+                }
+            }
+        }
+    }
+    return results;
+}
+
+
 PYBIND11_MODULE(signate131, m){
     m.doc() = "signate131 postprocess implementations";
     m.def("proc", &proc, "naive");
     m.def("proc2", &proc2, "x20");
     m.def("proc2_with_scale", &proc2_with_scale, "with scale");
+    m.def("proc3", &proc3, "x20");
 }
